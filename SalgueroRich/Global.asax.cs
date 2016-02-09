c060801abb51c8b26ac54d3209a293301ed177af
Tony Rodriguez <tonyruco@gmail.com>
2016-02-08 21:49:14 -0600

    test

--- SalgueroRich/Global.asax.cs
+++ SalgueroRich/Global.asax.cs
@@ -14,7 +14,7 @@ namespace SalgueroRich
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            RouteConfig.RegisterRoutes(RouteTable.Routes); 
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
